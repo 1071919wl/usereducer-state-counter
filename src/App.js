81837a3f352468698500e7c46a9cb8c3b29de9a3
William <w.leung.93@gmail.com>
2020-07-02 09:49:16 -0700

    reducer/state practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useReducer} from 'react';
+import CounterOne from './components/CounterOne'
+import CounterTwo from './components/CounterTwo'
+import CounterThree from './components/CounterThree'
+import ComponentA from './abcComponents/ComponentA'
+import ComponentB from './abcComponents/ComponentB'
+import ComponentC from './abcComponents/ComponentC'
+
+export const CountContext = React.createContext()
+
+const initialState = 0
+const reducer = (state, action) => {
+  switch(action){
+    case 'increment':
+      return state + 1
+    case 'decrement':
+      return state - 1
+    case 'reset':
+      return initialState
+    default:
+      return state
+  }
+}
+
 
 function App() {
+  const [count, dispatch] = useReducer(reducer, initialState)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CountContext.Provider
+      value={{countState: count, countDispatch: dispatch}}>
+      <div className="App">
+        Count - {count}
+        <CounterOne />
+        <CounterTwo />
+        <CounterThree />
+        <ComponentA />
+        <ComponentB />
+        <ComponentC />
+      </div>
+    </CountContext.Provider>
   );
 }
 
